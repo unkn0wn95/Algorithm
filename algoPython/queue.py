9e1bd357d8096989038adfe0890e14df3cd24dfb
unkn0wn95 <unkn0wn95@protonmail.com>
2022-08-01 00:34:14 +0800

    added dequeue

--- algoPython/queue.py
+++ algoPython/queue.py
@@ -26,7 +26,22 @@ class Queue:
             self.last = new_node
         self.length += 1
 
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        temp = self.first
+        if self.length == 1:
+            self.first = None
+            self.last = None
+        else:
+            self.first = self.first.next
+            temp.next = None
+        self.length -= 1
+        return temp
+
 my_queue = Queue(1)
 my_queue.enqueue(2)
 
-my_queue.print_queue()
+print(my_queue.dequeue())
+print(my_queue.dequeue())
+print(my_queue.dequeue())
