00e421bf218e37d48a7305ddb9c5c0e47334bb4a
unkn0wn95 <unkn0wn95@protonmail.com>
2022-08-01 00:22:00 +0800

    added enqueue

--- algoPython/queue.py
+++ algoPython/queue.py
@@ -15,7 +15,18 @@ class Queue:
         while temp is not None:
             print(temp.value)
             temp = temp.next
+    
+    def enqueue(self,value):
+        new_node = Node(value)
+        if self.first is None:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
 
-my_queue = Queue(4)
+my_queue = Queue(1)
+my_queue.enqueue(2)
 
 my_queue.print_queue()
