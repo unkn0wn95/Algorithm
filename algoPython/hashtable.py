5e06d78b49439c0bd59f0696d7a2267b887df828
unkn0wn95 <unkn0wn95@protonmail.com>
2022-08-03 01:04:33 +0800

    added get method

--- algoPython/hashtable.py
+++ algoPython/hashtable.py
@@ -17,11 +17,22 @@ class HashTable:
         if self.data_map[index] == None:
             self.data_map[index] = []
         self.data_map[index].append([key,value])
+    
+    def get_item(self,key):
+        index = self.__hash(key)
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+        return None
+
 
 my_hash_table = HashTable()
 
 my_hash_table.set_item('bolts',1400)
 my_hash_table.set_item('washers',50)
-my_hash_table.set_item('lumber',70)
 
-my_hash_table.print_table()
+
+print(my_hash_table.get_item('bolts'))
+print(my_hash_table.get_item('washers'))
+print(my_hash_table.get_item('lumber'))
