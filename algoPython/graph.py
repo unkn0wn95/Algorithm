04cb004ad3b436dbaf8d8e88c2e8ec1e0065bd28
unkn0wn95 <unkn0wn95@protonmail.com>
2022-08-03 18:08:15 +0800

    added add_edge method

--- algoPython/graph.py
+++ algoPython/graph.py
@@ -12,8 +12,18 @@ class Graph:
             return True
         return False
 
+    def add_edge(self,v1,v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+        return False
+
 my_graph = Graph()
 
-my_graph.add_vertex('A')
+my_graph.add_vertex(1)
+my_graph.add_vertex(2)
+
+my_graph.add_edge(1,2)
 
 my_graph.print_graph()
