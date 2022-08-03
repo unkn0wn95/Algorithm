342b6206210d2d6a1c3a18924e9076dc6119617e
unkn0wn95 <unkn0wn95@protonmail.com>
2022-08-03 15:34:05 +0800

    added keys method

--- algoPython/hashtable.py
+++ algoPython/hashtable.py
@@ -26,13 +26,19 @@ class HashTable:
                     return self.data_map[index][i][1]
         return None
 
+    def keys(self):
+        all_keys = []
+        for i in range(len(self.data_map)):
+            if self.data_map[i] is not None:
+                for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
+        return all_keys
+
 
 my_hash_table = HashTable()
 
 my_hash_table.set_item('bolts',1400)
 my_hash_table.set_item('washers',50)
+my_hash_table.set_item('lumber',70)
 
-
-print(my_hash_table.get_item('bolts'))
-print(my_hash_table.get_item('washers'))
-print(my_hash_table.get_item('lumber'))
+print(my_hash_table.keys())
