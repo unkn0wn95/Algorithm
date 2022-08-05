b280661735293dec899ec72ab09bab82fe1d5027
unkn0wn95 <unkn0wn95@protonmail.com>
2022-08-06 00:27:25 +0800

    added quick_sort_helper and quick_sort function

--- algoPython/quick_sort.py
+++ algoPython/quick_sort.py
@@ -13,8 +13,14 @@ def pivot(my_list,pivot_index,end_index):
     swap(my_list,pivot_index,swap_index)
     return swap_index
 
-my_list = [4,6,1,7,3,2,5]
+def quick_sort_helper(my_list,left,right):
+    if left < right:
+        pivot_index = pivot(my_list,left,right)
+        quick_sort_helper(my_list,left,pivot_index-1)
+        quick_sort_helper(my_list,pivot_index+1,right)
+    return my_list
 
-print(pivot(my_list,0,6))
+def quick_sort(my_list):
+    return quick_sort_helper(my_list,0,len(my_list)-1)
 
-print(my_list)
+print(quick_sort([4,6,1,7,3,2,5]))
