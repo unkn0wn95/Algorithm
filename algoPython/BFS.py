f66746eb395c4b5df5eada3d2204b84dc6d57a13
unkn0wn95 <unkn0wn95@protonmail.com>
2022-08-07 22:48:39 +0800

    added dfs_in_order method

--- algoPython/BFS.py
+++ algoPython/BFS.py
@@ -79,6 +79,16 @@ class BinarySearchTree:
         traverse(self.root)
         return results
 
+    def dfs_in_order(self):
+        results = []
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            results.append(current_node.value)
+            if current_node.right is not None:
+                traverse(current_node.right)
+        traverse(self.root)
+        return results
 my_tree = BinarySearchTree()
 my_tree.insert(47)
 my_tree.insert(21)
@@ -88,7 +98,7 @@ my_tree.insert(27)
 my_tree.insert(52)
 my_tree.insert(82)
 
-print(my_tree.dfs_post_order())
+print(my_tree.dfs_in_order())
 
 # [47, 21, 76, 18, 27, 52, 82]
 
